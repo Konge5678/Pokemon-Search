2b34d2e0d2a71c5e00ca1306f6436f7968d1b16f
Kristian Haugsrud <kris.haugsrud@gmail.com>
2024-05-08 11:31:26 +0200

    fixed the header and added an error if the name on the pokemon could not be found

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,39 @@
 import React, { useState } from 'react';
 
 function PokemonSearch() {
-  const [search, setSearch] = useState('');
   const [pokemon, setPokemon] = useState(null);
+  const [search, setSearch] = useState('');
+  const [error, setError] = useState(false);
 
   const handleChange = event => {
     setSearch(event.target.value);
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     fetch(`https://pokeapi.co/api/v2/pokemon/${search.toLowerCase()}`)
-      .then(response => response.json())
-      .then(data => setPokemon(data))
-      .catch(error => console.error('Error:', error));
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Pokemon not found');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setPokemon(data);
+        setError(false);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        setError(true);
+      });
   };
-
   return (
     
-  <div className="p-6 mt-4 max-w-sm mx-auto bg-gray-600 rounded-xl shadow-md flex flex-col items-center space-y-8 text-left">
-    <h1 className='text-3xl text-yellow-400 font-Pokemon'>POKE-SEARCH</h1>
+    <div className="p-6 mt-4 max-w-sm mx-auto bg-gray-600 rounded-xl shadow-md flex flex-col items-center space-y-8 text-left">
+    <h1 className='text-3xl text-yellow-400 font-Pokemon style [text-shadow:_0_5px_0_rgb(0_0_0_/_80%)] '>POKE-SEARCH</h1>
     <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
       <input className='bg-gray-400 p-2 rounded-lg' type="text" value={search} onChange={handleChange} placeholder="Enter Pokemon name" />
+      {error && <p className="text-red-500">Pokemon could not be found</p>}
       <button className='text-blue-400 bg-black p-2 rounded-lg' type="submit">Search</button>
     </form>
     {pokemon && (
